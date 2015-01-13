03ed994363136257507da179ff89994d0ccfb05d
XOR19 <nils.emmerich@web.de>
2015-01-13 11:05:01 +0100

    Things to add

--- src/xscript/compiler/XCompilerOptions.java
+++ src/xscript/compiler/XCompilerOptions.java
@@ -4,6 +4,11 @@ import java.util.Map;
 
 public class XCompilerOptions {
 
+public boolean addLines;
+public int optimizionLevel;
+public boolean removeAsserts;
+
+
 	public void from(Map<String, Object> options){
 		
 	}
